444d33bfcd457ab808836045ba01d43e72fd10d5
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-06-02 11:28:32 -0500

    commit tree dd41c9911693e79aa72c6e19405edc8487d9c41f

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Write your solution here!
+function reset() {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  }
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+reset();
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+reset();
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+reset();
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+reset();
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+reset();
+
+function appendCat(name) {
+    const newcats = [...cats, name];
+    return newcats
+}
+
+appendCat("Broom");
+
+reset();
+
+function prependCat(name) {
+    const newcats = [name, ...cats];
+    return newcats
+}
+
+appendCat("Arnold");
+
+reset();
+
+function removeLastCat() {
+    const newcats = cats.slice(0, 2);
+    return newcats
+}
+
+reset();
+
+function removeFirstCat() {
+    const newcats = cats.slice(1);
+    return newcats
+}
